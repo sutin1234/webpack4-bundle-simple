837e839db4971c836407d913c0efc686fd54e78f
sutin <tony.sutin1234@gmail.com>
2019-07-18 10:10:47 +0700

    add modernizr & detectizr

---
+++ src/js/bootstrap.js
@@ -0,0 +1,3 @@
+import '../scss/main.scss'
+//import * as Modernizr from '../vendors/modernizr-custom';
+import * as Detectizr from 'modernizr-detectizr/libs/modernizr-detectizr.min'
--- webpack.config.js
+++ webpack.config.js
@@ -3,9 +3,11 @@ const path = require('path')
 const MiniCssExtractPlugin = require("mini-css-extract-plugin")
 const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin')
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 // config
-const entryFile = path.resolve(__dirname, 'src/js/index.js')
+const entryFile = path.resolve(__dirname, 'src/js/app.js')
+const outputFile = 'bundle.js'
 
 
 
@@ -13,7 +15,14 @@ module.exports = {
     mode: process.env.NODE_ENV,
     entry: entryFile,
     output: {
-        filename: 'bundle.js'
+        filename: outputFile,
+        path: path.resolve(__dirname, 'dist')
+    },
+    resolve: {
+        extensions: ['.js', '.css', '.scss', '.sass', '.less', '.png'],
+        alias: {
+            modernizr$: path.resolve(__dirname, "src/vendors/modernizr-custom.js")
+        }
     },
     optimization: {
         minimizer: [
@@ -34,24 +43,41 @@ module.exports = {
     },
     module: {
         rules: [{
-            test: /\.scss$/,
-            use: [
-                process.env.NODE_ENV !== 'production' ? 'style-loader' : MiniCssExtractPlugin.loader,
-                {
-                    loader: 'css-loader',
-                    options: {
-                        importLoaders: 1,
-                        minimize: true
-                    }
-                },
-                "sass-loader"
-            ]
-        }]
+                test: /\.scss$/,
+                use: [
+                    process.env.NODE_ENV !== 'production' ? 'style-loader' : MiniCssExtractPlugin.loader,
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            importLoaders: 1,
+                            minimize: true
+                        }
+                    },
+                    "postcss-loader",
+                    "sass-loader",
+
+                ]
+            },
+            {
+                test: /modernizr/,
+                loader: 'imports-loader?this=>window!exports-loader?window.Modernizr'
+            },
+            {
+                test: /detectizr/,
+                loader: 'imports-loader?this=>window!exports-loader?window.Detectizr'
+            }
+        ]
     },
     plugins: [
         new MiniCssExtractPlugin({
             filename: "[name].css",
             chunkFilename: "[id].css"
-        })
+        }),
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, 'index.html'),
+            minify: {
+                collapseWhitespace: true
+            },
+        }),
     ],
 };
